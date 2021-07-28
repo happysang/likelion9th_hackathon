19825558dcfc5f966c64a53c2290c38344ab4b9b
hoony0125 <kkh8748154@gmail.com>
2021-07-28 19:51:21 +0900

    review
    detail 및 readall 페이지 기본 기능구현

--- hospital_project/review/urls.py
+++ hospital_project/review/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from review.views import *
 
 urlpatterns = [
-    
+    path('review/detail<str:each_id>',review_detail_view,name='urlreviewdetail'),
+    path('readall',review_readall_view,name='urlreviewreadall'),
 ]
