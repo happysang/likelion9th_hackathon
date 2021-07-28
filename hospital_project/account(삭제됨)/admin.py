9e363fa0807664c336e7cb3ed36292b08a38da1a
khs <khssee930@likelion.org>
2021-07-29 00:32:10 +0900

    account삭제
    .

--- hospital_project/account(삭제됨)/admin.py
+++
@@ -1,5 +0,0 @@
-from account.models import CustomUser
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(CustomUser)
--- hospital_project/account(삭제됨)/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from account.views import *
-
-urlpatterns = [
-    path('login/', login_view, name = 'urllogin'),
-    path('signup/<str:c>', signup_view, name = 'urlsignup'),
-    path('choice/', choice, name = 'urlchoice')
-]
