6a0ae68d147a52ffdd4712369b02e0d0cb977b83
hongsikcho <jjdd9758@naver.com>
2021-07-29 01:12:38 +0900

    information 기본템플릿

--- hospital_project/information/urls.py
+++ hospital_project/information/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from information.views import *
 
 urlpatterns = [
-    
+    path('', InfoList ,name = "urlInfoList"),
+    path('write/' , write , name ="urlwrite"),
+    path('create/' , create , name="urlcreate"),
 ]
--- hospital_project/information/views.py
+++ hospital_project/information/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+#from hospital_project.hospital_project.settings import TIME_ZONE
+from django.shortcuts import render,redirect,get_list_or_404
+from .models import Info
+from django.utils import timezone 
 
 # Create your views here.
+
+def InfoList(request):
+    Infos = Info.objects.all()
+    return render(request, 'InfoList.html' , {'Infos':Infos})
+
+
+def write(request):
+    return render(request , 'write.html')
+
+def create(request):
+    new_Info = Info()
+    new_Info.dept = request.POST['dept']
+    new_Info.user_id = request.POST['user_id']
+    new_Info.date = timezone.now()
+    new_Info.title = request.POST['title']
+    new_Info.body = request.POST['body']
+    new_Info.save()
+    return redirect('urlInfoList')
+
+
