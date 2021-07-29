a56bd85a072d8dbb2516c27fd8059702b451df84
tymorehard <tsi04037@likelion.org>
2021-07-29 19:21:25 +0900

    test
    test

--- hospital_project/information/models.py
+++ hospital_project/information/models.py
@@ -1,3 +1,3 @@
 from django.db import models
-
+from django.db.models.fields import CharField
 # Create your models here.
--- hospital_project/information/urls.py
+++ hospital_project/information/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from information.views import *
 
 urlpatterns = [
+    path('info', info, name = 'urlnameinfo'),
+    path('readall', readall, name = 'urlnamereadall'),
+    path('infodetail/<str:each_id>', infodetail, name = 'urlnameinfodetail'),
     
+
 ]
