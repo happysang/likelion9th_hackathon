fd7914c71f6b0ea552dcba6b39b0fb1b0a75c4c9
seoZ02 <87064616+seoZ02@users.noreply.github.com>
2021-07-30 07:02:10 +0900

    create url
    new,create url작성

--- hospital_project/review/urls.py
+++ hospital_project/review/urls.py
@@ -4,4 +4,6 @@ from review.views import *
 urlpatterns = [
     path('review/detail<str:each_id>',review_detail_view,name='urlreviewdetail'),
     path('readall',review_readall_view,name='urlreviewreadall'),
+    path('new/', review_new_view, name='urlreviewnew'),
+    path('create/', review_create_view, name='urlreviewcreate')
 ]
