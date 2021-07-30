2f53272ccd6bfc8c8cbe49743932e2c1ff7670ac
hoony0125 <kkh8748154@gmail.com>
2021-07-30 20:05:49 +0900

    review detail/readall/create 합본
    ㅈㄱㄴ

--- hospital_project/review/models.py
+++ hospital_project/review/models.py
@@ -13,6 +13,6 @@ class Review(models.Model):
     dept = models.CharField(max_length=20)
     body = models.TextField(max_length=300)
     cert = models.ImageField(upload_to="review/", blank=True, null =True)
-
+ 
 def __str__(self):
     return self.title
