2f2017251468778cc3654ec3f200ba644418e8b3
hongsikcho <jjdd9758@naver.com>
2021-07-30 17:49:31 +0900

    CRUD완성 및 DELET의 알람기능까지요

--- hospital_project/information/urls.py
+++ hospital_project/information/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('write/' , write , name ='urlwrite'),
     path('create/' , create , name='urlcreate'),
     path('detail/<str:Info_id>', detail, name='urldetail'),
+    path('edit/<str:id>' , edit , name="urledit"),
+    path('update/<str:id>' , update , name = "urlupdate"),
+    path('delete/<str:id>' , delete , name = 'urldelete'),
+   
 ]
--- hospital_project/information/views.py
+++ hospital_project/information/views.py
@@ -24,3 +24,25 @@ def create(request):
 def detail(request, Info_id):
     info_detail = get_object_or_404(Info, pk= Info_id)
     return render (request, 'detail.html', {'info': info_detail})
+
+
+def edit(request , id):
+    edit_info = Info.objects.get(id = id)
+    return render(request , 'edit.html' , {'info':edit_info})
+
+def update(request,id):
+    update_info = Info.objects.get(id =id)
+    update_info.dept = request.POST['dept']
+    update_info.user_id = request.POST['user_id']
+    update_info.date = timezone.now()
+    update_info.title = request.POST['title']
+    update_info.body = request.POST['body']
+    update_info.save()
+    return redirect('urldetail' , update_info.id)
+
+def delete(request , id):
+    delete_info = Info.objects.get(id = id)
+    delete_info.delete()
+    return redirect('urlInfoList')
+
+
