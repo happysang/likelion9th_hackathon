0ad7e1c661b3ee334234bd97242cf84cf2c8419c
narin727 <rhkrskfls@naver.com>
2021-07-31 17:16:41 +0900

    질문보기편집

--- hospital_project/question/urls.py
+++ hospital_project/question/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from question.views import *
 
 urlpatterns = [
-    
+    path('',home,name="urlnamehome"),
+    path('allq/', allq, name="urlnameallq"),
+    path('<str:id>',detail,name="urlnamedetail"),
+    path('edit/<str:id>',edit,name="urlnameedit"),
+    path('update/<str:id>',update,name="urlnameupdate"),
+    path('question/comment/<str:id>', add_comment, name='urlnameadd_comment'),
+
 ]
--- hospital_project/question/views.py
+++ hospital_project/question/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.core import paginator
+from django.db.models.query import QuerySet
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Question
+from django.utils import timezone
+from django.core.paginator import Paginator
+from .forms import CommentForm
 
 # Create your views here.
+
+def home(request):
+    return render (request, 'home.html')
+
+def allq(request):
+    questions = Question.objects.order_by('-date')
+    paginator = Paginator(questions,10)
+    page = request.GET.get('page')
+    questions = paginator.get_page(page)
+    return render(request,'allq.html',{'questions':questions})
+
+def detail(request,id):
+    question = get_object_or_404(Question, pk=id)
+    default_view_count = question.view_count
+    question.view_count = default_view_count +1 
+    question.save()
+    return render(request,'detail.html',{'question':question})
+
+def edit(request,id):
+    edit_question = Question.objects.get(id=id)
+    return render(request,'edit.html',{'question':edit_question})
+
+def update(request,id):
+    update_question = Question.objects.get(id=id)
+    update_question.title = request.POST['htmltitle']
+    update_question.dept = request.POST['htmldept']
+    update_question.user_id = request.POST['htmluser_id']
+    update_question.body = request.POST['htmlbody']
+    update_question.date = timezone.now()
+    update_question.save()
+    return redirect('urlnamedetail', update_question.id)
+
+def add_comment(request, id): 
+    question=get_object_or_404(Question, pk=id) 
+    if request.method == "POST":
+         form=CommentForm(request.POST) 
+         if form.is_valid(): 
+             comment=form.save(commit=False) 
+             comment.post= question 
+             comment.save() 
+         return redirect('urlnamedetail',id) 
+    else: 
+        form=CommentForm() 
+    return render(request, 'add_comment.html', {'form':form})
+
