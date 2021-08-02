799c220e690233e2721009910304d2448a20cf5a
Imseonju <wnwn37905@likelion.org>
2021-08-03 03:20:41 +0900

    mypage
    .

--- hospital_project/etc/urls.py
+++ hospital_project/etc/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from etc.views import *
 
 urlpatterns = [
-    path('',home, name = 'urlhome')
+    path('',home, name = 'urlhome'),
+    path('mypage/', mypage, name="urlmypage"),
+    path('mypagedetail/', mypagedetail, name="urlmypagedetail"),
 ]
--- hospital_project/etc/views.py
+++ hospital_project/etc/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from inner_account.models import CustomUser
 
 # Create your views here.
 def home(request):
     return render (request, 'home.html')
+
+def mypage(request):
+    return render(request,'mypage.html')
+
+def mypagedetail(request):
+    return render(request, 'mypagedetail.html')
