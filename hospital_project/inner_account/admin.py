c77f58e2b80a0e741fbf24972037deb306ed595e
khs <69028828+happysang@users.noreply.github.com>
2021-08-12 02:16:42 +0900

    .
    .

--- hospital_project/inner_account/admin.py
+++ hospital_project/inner_account/admin.py
@@ -2,4 +2,9 @@ from inner_account.models import CustomUser
 from django.contrib import admin
 
 # Register your models here.
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'cert','ans_auth', 'date_joined']
+    list_display_links = ['username']
+    search_fields = ['username'] # 아이디로 검색 가능
