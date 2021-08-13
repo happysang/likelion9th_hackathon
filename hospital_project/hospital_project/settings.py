b59ea4a067dd9dc1b8d4f7fc807e496fdb89927b
khs <69028828+happysang@users.noreply.github.com>
2021-08-13 19:53:01 +0900

    final commit

--- hospital_project/hospital_project/settings.py
+++ hospital_project/hospital_project/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -170,3 +171,8 @@ EMAIL_HOST_USER = 'byeonguibogam@gmail.com'
 EMAIL_HOST_PASSWORD = 'quddmlqhrka'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
